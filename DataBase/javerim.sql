ff0f8caf8f5af3eabd885fd59e3341faaf885e5d
ricardo <ricardosantilo96@gmail.com>
2019-06-07 18:55:48 -0500

    .

--- DataBase/javerim.sql
+++ DataBase/javerim.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-05-2019 a las 00:03:42
+-- Tiempo de generación: 08-06-2019 a las 01:55:05
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
@@ -45,7 +45,9 @@ CREATE TABLE `alumnos` (
 INSERT INTO `alumnos` (`id_alumno`, `nombre_alumno`, `cel_alumno`, `correo_alumno`, `carrera_alumno`, `semestre_alumno`, `contrasena_alumno`) VALUES
 (1, 'Ricardo N', '5576778990', 'ricardo@email.com', 'Ingeniaría en Computación', 8, 'yomeroR'),
 (2, 'Hannia N', '5576758990', 'hannia@email.com', 'Ingeniaría en Computación', 6, 'yomeroH'),
-(3, 'Benjamín N', '5576758910', 'benja@email.com', 'Ingeniaría Eléctrica Electrónica', 8, 'yomeroB');
+(3, 'Benjamín N', '5576758910', 'benja@email.com', 'Ingeniaría Eléctrica Electrónica', 8, 'yomeroB'),
+(4, 'Ricardo', '5517496889', 'otroricardo@email.com', 'IngenierÃ­a en ComputaciÃ³n', 1, '$2y$10$74n2f8L0rm0VyrmZwBJhJupG60gkUAzlIun9VEo0tzGb9nv9tjabm'),
+(5, 'Ricardo', '5548986454', 'richard@correo.com', 'IngenierÃ­a en ComputaciÃ³n', 3, '$2y$10$0lokCkUY5ZgLvm0CvPZwG.tIGtADx4HKXE9ql2gNIeOfwKS7N0qtS');
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,10 @@ INSERT INTO `asesor` (`id_asesor`, `nombre_asesor`, `celular_asesor`, `correo_as
 (7, 'Nayeli N', '5563273214', 'nayeli@correo.com', 'Ingeniería Mecatrónica', 7, 'asesor7'),
 (8, 'Bryan N', '5523273214', 'bryan@correo.com', 'Ingeniería Civil', 8, 'asesor8'),
 (9, 'Reina N', '5523273239', 'reina@correo.com', 'Ingeniería Petrolera', 10, 'asesor9'),
-(10, 'Mario N', '5523273239', 'reina@correo.com', 'Ingeniería Industrial', 6, 'asesor10');
+(10, 'Mario N', '5523273239', 'reina@correo.com', 'Ingeniería Industrial', 6, 'asesor10'),
+(11, 'Abel', '5545632533', 'abeli@umail.com', 'IngenierÃ­a Civil', 9, '$2y$10$dB0Aqt1x4TZV6NzRlKoT9uiLo7OY2ir7K0biKvdgeg55Usb8vOKe.'),
+(12, 'Sandra', '5589624878', 'sandy09@correo.com', 'IngenierÃ­a de Minas y Metalurgia', 5, '$2y$10$wdH8508YPpdtV47IL0Pyw.y9bEvxGXymWxtyTN/0zgB/zbtQKInIq'),
+(13, 'Karla Rosas123546', '55355555555555555456666566666666666666665555555555555555555555555555555555555555', 'karla.charlot19@gmail.com', 'IngenierÃ­a en ComputaciÃ³n', 10, '$2y$10$URRB8L/tu46NY6qHINBBpuFE3B2e4SXCe/uK5ABgwI/FqwsoY19Pe');
 
 -- --------------------------------------------------------
 
@@ -100,8 +105,16 @@ CREATE TABLE `cita` (
 --
 
 INSERT INTO `cita` (`id_cita`, `fecha_cita`, `hora_cita`, `nombre_materia`, `lugar_cita`, `id_alumno`, `id_asesor`) VALUES
-(9, '31-05-2019', '08:30:00', 'Cinematica y Dinamica', 'Biblioteca Antonio Dovali Jaime', 2, 8),
-(10, '31-05-2019', '18:00:00', 'Geometria Analitica', 'Cafe 76', 2, 10);
+(14, '06-06-2019', '16:30:00', 'Algebra', 'Bancas del Principal', 5, 10),
+(15, '06-06-2019', '16:30:00', 'Algebra', 'Bancas del Principal', 5, 10),
+(16, '06-06-2019', '16:30:00', 'Algebra', 'Bancas del Principal', 5, 10),
+(17, '07-06-2019', '08:30:00', 'Cinematica y Dinamica', 'Biblioteca Antonio Dovali Jaime', 5, 8),
+(18, '06-06-2019', '11:45:00', 'Ecuaciones Diferenciales', 'biblioteca Principal', 5, 11),
+(19, '07-06-2019', '11:45:00', 'Cinematica y Dinamica', 'jardÃ­n Valdez Vallejo', 5, 11),
+(20, '07-06-2019', '11:45:00', 'Ecuaciones Diferenciales', 'islas', 5, 11),
+(21, '06-06-2019', '16:30:00', 'Quimica', 'Bancas del Principal', 5, 10),
+(22, '06-06-2019', '11:45:00', 'Matematicas Avanzadas', 'biblioteca Principal', 5, 11),
+(23, '07-06-2019', '11:45:00', 'Matematicas Avanzadas', 'islas', 5, 11);
 
 -- --------------------------------------------------------
 
@@ -112,7 +125,6 @@ INSERT INTO `cita` (`id_cita`, `fecha_cita`, `hora_cita`, `nombre_materia`, `lug
 CREATE TABLE `clase` (
   `id_asig` int(80) NOT NULL,
   `nombre_asig` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
-  `Tema_asig` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
   `semstre_asig` int(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
@@ -120,17 +132,17 @@ CREATE TABLE `clase` (
 -- Volcado de datos para la tabla `clase`
 --
 
-INSERT INTO `clase` (`id_asig`, `nombre_asig`, `Tema_asig`, `semstre_asig`) VALUES
-(2, 'Algebra', 'Matrices', 1),
-(3, 'Algebra', 'Conjuntos', 1),
-(4, 'Calculo Diferencial', 'Limites', 1),
-(5, 'Calculo Diferencial', 'Derivadas', 1),
-(6, 'Calculo Integral', 'Métodos de integración', 2),
-(7, 'Calculo Integral', 'Función exponencial', 2),
-(8, 'Cinematica y Dinamica', 'Cinemática de la partícula', 3),
-(9, 'Ecuaciones Diferenciales', 'Ecuaciones diferenciales lineales', 3),
-(10, 'Ecuaciones Diferenciales', 'Transformada de Laplace', 3),
-(11, 'Geometria Analitica', 'Curvas en el plano polar', 1);
+INSERT INTO `clase` (`id_asig`, `nombre_asig`, `semstre_asig`) VALUES
+(2, 'Algebra', 1),
+(3, 'Quimica', 1),
+(4, 'Calculo Diferencial', 1),
+(5, 'Algebra lineal', 1),
+(6, 'Calculo Integral', 2),
+(7, 'Calculo Vectorial', 2),
+(8, 'Cinematica y Dinamica', 3),
+(9, 'Ecuaciones Diferenciales', 3),
+(10, 'Matematicas Avanzadas', 3),
+(11, 'Geometria Analitica', 1);
 
 -- --------------------------------------------------------
 
@@ -153,14 +165,25 @@ CREATE TABLE `sesiones` (
 --
 
 INSERT INTO `sesiones` (`id_sesion`, `dia_semana`, `horario_sesion`, `lugar_sesion`, `Estado`, `id_asesor`, `id_clase`) VALUES
-(1, 'Viernes', '18:00:00', 'Cafe 76', 1, 10, 11),
+(1, 'Viernes', '18:00:00', 'Cafe 76', 0, 10, 11),
 (2, 'Lunes', '11:00:00', 'Banquitas del J', 0, 1, 5),
 (3, 'Martes', '12:45:00', 'La Leonardita (Pokebola Anexo)', 0, 4, 2),
-(4, 'Jueves', '16:30:00', 'Bancas del Principal', 0, 10, 3),
+(4, 'Jueves', '16:30:00', 'Bancas del Principal', 1, 10, 3),
 (5, 'Miercoles', '12:45:00', 'Biblioteca Enrique Rivero Borrell', 0, 9, 9),
 (6, 'Martes', '13:15:00', 'Banquitas bajo el I', 0, 3, 6),
 (7, 'Viernes', '08:30:00', 'Biblioteca Antonio Dovali Jaime', 1, 8, 8),
-(8, 'Miercoles', '12:00:00', 'jardin del Postgrado', 0, 7, 2);
+(8, 'Miercoles', '12:00:00', 'jardin del Postgrado', 0, 7, 2),
+(9, 'Viernes', '11:45:00', 'jardÃ­n Valdez Vallejo', 1, 11, 8),
+(10, 'Jueves', '11:45:00', 'biblioteca Principal', 1, 11, 10),
+(11, 'Viernes', '11:45:00', 'islas', 1, 11, 10),
+(12, 'Lunes', '11:45:00', 'Banquitas del J', 0, 11, 4),
+(13, 'MiÃ©rcoles', '18:30:00', 'JardÃ­n del Postgrado', 0, 11, 4),
+(14, 'MiÃ©rcoles', '11:45:00', 'CubÃ­culos Biblioteca Anexo', 0, 11, 10),
+(15, 'Jueves', '11:45:00', 'Frente a Biblioteca Central', 0, 11, 3),
+(16, 'SÃ¡bado', '11:45:00', 'La Leonardita', 0, 12, 6),
+(17, 'SÃ¡bado', '13:45:00', 'La Leonardita', 0, 12, 5),
+(18, 'Viernes', '13:00:00', 'biblioteca Principal', 0, 13, 2),
+(19, 'Jueves', '11:45:00', 'biblioteca Principal', 0, 13, 6);
 
 --
 -- Índices para tablas volcadas
@@ -208,19 +231,19 @@ ALTER TABLE `sesiones`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id_alumno` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_alumno` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `asesor`
 --
 ALTER TABLE `asesor`
-  MODIFY `id_asesor` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_asesor` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `cita`
 --
 ALTER TABLE `cita`
-  MODIFY `id_cita` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_cita` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `clase`
@@ -232,7 +255,7 @@ ALTER TABLE `clase`
 -- AUTO_INCREMENT de la tabla `sesiones`
 --
 ALTER TABLE `sesiones`
-  MODIFY `id_sesion` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_sesion` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
