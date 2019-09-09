c858c06741f9103ad059c17940894b94872171d2
HanniHans <HanniHans@github.com>
2019-09-08 23:11:57 -0500

    acomodo de footer

--- source/footer.php
+++ source/footer.php
@@ -1,4 +1,9 @@
+<br/>
+<br/>
+<br/>
 <footer class="page-footer font-small fixed-bottom" style="background-color: #1976D2;">
+ 
+ 
   <!-- Copyright -->
   <div class="footer-copyright text-center py-1" style="color:#FFFFFF";>© 2019 Copyright:
     <a href="http://mobile.unam.mx/javerim/" style="color:#FFFFFF";> UNAM Mobile</a>
